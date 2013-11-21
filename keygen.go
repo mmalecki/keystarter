566b2b40b88e73afd58ac3aee3c7864a33bf0ba0
Maciej Małecki <me@mmalecki.com>
2013-11-21 12:59:44 +0100

    Generate a private key

--- keygen.go
+++ keygen.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-  "fmt"
+  "os/exec"
+  "errors"
 )
 
-func Keygen() {
-  fmt.Println("Keygen!")
+var openssl string = "openssl"
+
+func Keygen() (error) {
+  cmd := exec.Command(openssl, "genrsa", "-out", "server.key", "4096")
+  err := cmd.Run()
+  if err != nil {
+    return errors.New("Generating a private key failed")
+  }
+
+  return nil
 }

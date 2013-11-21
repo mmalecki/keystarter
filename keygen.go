c0f260c36c4b49db7b420ebad2200165020ea43b
Maciej Małecki <me@mmalecki.com>
2013-11-21 13:02:07 +0100

    Move private key generation to a separate function

--- keygen.go
+++ keygen.go
@@ -7,9 +7,13 @@ import (
 
 var openssl string = "openssl"
 
-func Keygen() (error) {
+func privateKey() (error) {
   cmd := exec.Command(openssl, "genrsa", "-out", "server.key", "4096")
-  err := cmd.Run()
+  return cmd.Run()
+}
+
+func Keygen() (error) {
+  err := privateKey()
   if err != nil {
     return errors.New("Generating a private key failed")
   }
